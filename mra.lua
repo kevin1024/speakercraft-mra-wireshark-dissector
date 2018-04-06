904c7162d53f34f07c2ebf127487bb41a4b18e26
Kevin McCarthy <me@kevinmccarthy.org>
2018-04-05 22:08:58 -0500

    ghetto packet parsing works

--- mra.lua
+++ mra.lua
@@ -7,7 +7,7 @@ local commands = {
     [0x03] = "Get Audio Sense State ",
     [0x21] = "Get Current Volume",
     [0x23] = "Get Tone Control",
-    [0x25] = "Get Do Not Distrub",
+    [0x25] = "Get Do Not Disturb",
     [0x26] = "Set Routing Map",
     [0x27] = "Get Routing Map",
 }
@@ -35,6 +35,11 @@ f.audio_sense_5 = ProtoField.new("Audio 5", "audiosense.5", ftypes.BOOLEAN, {"On
 f.audio_sense_6 = ProtoField.new("Audio 6", "audiosense.6", ftypes.BOOLEAN, {"On","Off"}, 8, 0x04)
 f.audio_sense_paging = ProtoField.new("Paging Input", "audiosense.paging_input", ftypes.BOOLEAN, {"1","0"}, 8, 0x02)
 
+f.audio_input = ProtoField.uint8("mra.routing_map_input","Audio Input")
+f.audio_output = ProtoField.uint8("mra.routing_map_output","Audio Output")
+
+f.zone_output = ProtoField.uint8("mra.zone_output","Zone Output")
+
 f.major_version = ProtoField.uint8("mra.major_version", "Major Version Number")
 f.minor_version = ProtoField.uint8("mra.minor_version", "Minor Version Number")
 f.subversion = ProtoField.uint8("mra.subversion", "Subversion number")
@@ -61,38 +66,63 @@ function MRAPROTO.dissector (buffer, pinfo, tree)
     local command = buffer (offset, 1)
     subtree:add (f.command, command)
     offset = offset + 1
+    
+    local request = pinfo.dst_port == 10200
+    local response = pinfo.dst_port ~= 10200
+    local result = 0
+    local data = false
+    local data_tree = nil
 
-    if (length:uint() > 1) then
+    if (response) then
 	local result = buffer(offset, 1)
-        offset = offset + 1
 	subtree:add(f.result, result)
-        local data = buffer (offset, length:uint() - 2)
-	local data_tree = subtree:add (f.data, data)
-        offset = offset + length:uint() - 2
-
-	if (command:uint() == 3 and result:uint()==1) then
-            data_tree:add(f.audio_sense_1, data)
-            data_tree:add(f.audio_sense_2, data)
-            data_tree:add(f.audio_sense_3, data)
-            data_tree:add(f.audio_sense_4, data)
-            data_tree:add(f.audio_sense_5, data)
-            data_tree:add(f.audio_sense_6, data)
-            data_tree:add(f.audio_sense_paging, data)
-        end
-
-	if (command:uint() == 0 and result:uint()==1) then
-            data_tree:add(f.major_version, data:range(0,1))
-            data_tree:add(f.minor_version, data:range(1,1))
-            data_tree:add(f.subversion, data:range(2,1))
-            data_tree:add(f.build, data:range(3,1))
-	end
-
+        offset = offset + 1
+	result = result:uint()
     end
+
+    if (result and length:uint()>2 or not result and length:uint()>1) then
+        data = buffer (offset, length:uint() - 2)
+	data_tree = subtree:add (f.data, data)
+        offset = offset + length:uint() - 2
+    end
+
+    if (request and length:uint() > 1) then
+        data = buffer (offset, length:uint() - 1)
+	data_tree = subtree:add (f.data, data)
+        offset = offset + length:uint() - 2
+    end
+
+    if (response and command:uint() == 3 and result==1) then
+        data_tree:add(f.audio_sense_1, data)
+        data_tree:add(f.audio_sense_2, data)
+        data_tree:add(f.audio_sense_3, data)
+        data_tree:add(f.audio_sense_4, data)
+        data_tree:add(f.audio_sense_5, data)
+        data_tree:add(f.audio_sense_6, data)
+        data_tree:add(f.audio_sense_paging, data)
+    end
+
+    if (response and command:uint() == 0) then
+        data_tree:add(f.major_version, data:range(0,1))
+        data_tree:add(f.minor_version, data:range(1,1))
+        data_tree:add(f.subversion, data:range(2,1))
+        data_tree:add(f.build, data:range(3,1))
+    end
+
+    if (request and command:uint() == 0x21) then
+	subtree:add(f.zone_output, data:range(0,1))
+--	offset = offset + 1
+    end
+
+    if (request and command:uint() == 0x26) then 
+        data_tree:add(f.audio_input, data:range(0,1))
+--	offset = offset + 1
+        data_tree:add(f.audio_output, data:range(1,1))
+    end
+
 
     local checksum = buffer (offset, 1)
     subtree:add (f.checksum, checksum)
-
-
 end
 
 tcp_table = DissectorTable.get ("tcp.port")
